9dca29c8f441b64d7180948a08631d5a53834921
Max <smith.w2055@gmail.com>
2026-07-22 07:21:41 +0800

    Move diagnostics out of factory

--- src/core/Diagnostics.cpp
+++ src/core/Diagnostics.cpp
@@ -161,7 +161,7 @@ Base::Result<Diagnostic> Diagnostic::TryCreate(
     diagnostic.source_ = source;
     diagnostic.object_ = object;
     diagnostic.member_ = member;
-    return diagnostic;
+    return std::move(diagnostic);
 }
 
 Base::Result<void> Diagnostic::TryAddNote(
