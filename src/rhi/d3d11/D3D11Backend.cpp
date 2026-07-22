1cb4403edbea8072c9e52c7aa750a800706a1b21
Max <smith.w2055@gmail.com>
2026-07-22 09:20:20 +0800

    Assemble native D3D11 backend

---
+++ src/rhi/d3d11/D3D11Backend.cpp
@@ -0,0 +1,10 @@
+#include "D3D11BackendPrivate.hpp"
+
+namespace Aero::Rhi {
+
+#include "D3D11BackendDevice.inc"
+#include "D3D11BackendResources.inc"
+#include "D3D11BackendCommands.inc"
+#include "D3D11BackendReadback.inc"
+
+} // namespace Aero::Rhi
