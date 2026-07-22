1f8dad8a919c59fa3934621040e5f3f1304c5662
Max <smith.w2055@gmail.com>
2026-07-22 09:25:31 +0800

    Split D3D11 command translation sections

--- src/rhi/d3d11/D3D11Backend.cpp
+++ src/rhi/d3d11/D3D11Backend.cpp
@@ -4,7 +4,9 @@ namespace Aero::Rhi {
 
 #include "D3D11BackendDevice.inc"
 #include "D3D11BackendResources.inc"
-#include "D3D11BackendCommands.inc"
+#include "D3D11BackendCommands1.inc"
+#include "D3D11BackendCommands2.inc"
+#include "D3D11BackendCommands3.inc"
 #include "D3D11BackendReadback.inc"
 
 } // namespace Aero::Rhi
