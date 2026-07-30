30c637cd3752a3f99d4fd6b4a2da4809fff8de77
Max <smith.w2055@gmail.com>
2026-07-30 15:54:46 +0800

    add WPF dependency object surface

---
+++ include/Aero/DependencyObject.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Aero/Core/Property/DependencyProperty.hpp>
+
+namespace Aero {
+
+using DependencyObject = Core::DependencyObject;
+using DependencyProperty = Core::DependencyProperty;
+using DependencyPropertyKey = Core::DependencyPropertyKey;
+using DependencyPropertyHandle = Core::DependencyPropertyHandle;
+using DependencyPropertyChangedEventArgs =
+    Core::DependencyPropertyChangedEventArgs;
+using DependencyPropertyChangedEventHandler =
+    Core::DependencyPropertyChangedEventHandler;
+using PropertyMetadata = Core::PropertyMetadata;
+using PropertyMetadataFlags = Core::PropertyMetadataFlags;
+using UpdateSourceTrigger = Core::UpdateSourceTrigger;
+
+} // namespace Aero
