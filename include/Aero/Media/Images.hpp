81a7b4d13164d41fb4f7146bf7185a0ce622b562
Max <smith.w2055@gmail.com>
2026-07-30 15:21:08 +0800

    add media images namespace entry

---
+++ include/Aero/Media/Images.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Aero/Presentation/Images.hpp>
+
+namespace Aero::Media {
+
+using Stretch = ::Aero::Presentation::Stretch;
+using StretchDirection = ::Aero::Presentation::StretchDirection;
+using ImageSource = ::Aero::Presentation::ImageSource;
+using BitmapImage = ::Aero::Presentation::BitmapImage;
+
+} // namespace Aero::Media
