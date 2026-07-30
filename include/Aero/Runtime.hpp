3d6ec1f46a802814abb09942935aa3b2a532b83a
Max <smith.w2055@gmail.com>
2026-07-30 15:11:13 +0800

    remove runtime sdk type-definition macro

--- include/Aero/Runtime.hpp
+++ include/Aero/Runtime.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#define AERO_SDK_SURFACE_ONLY 1
+// Runtime is a stable include aggregation only. It must never change the
+// definition of public types through preprocessor feature macros.
 #include <Aero/RuntimeEnvironment.hpp>
 #include <Aero/RuntimeTypes.hpp>
 #include <Aero/UiDocument.hpp>
-#undef AERO_SDK_SURFACE_ONLY
