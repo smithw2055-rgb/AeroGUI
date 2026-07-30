2f6e76ed6ba2e6760ab118e0a7a09fb4d9916289
Max <smith.w2055@gmail.com>
2026-07-30 15:43:49 +0800

    add WPF resource and geometry surfaces

---
+++ include/Aero/Resources.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Aero/Presentation/Resources.hpp>
+
+namespace Aero {
+
+using NameScope = Presentation::NameScope;
+using ResourceKeyKind = Presentation::ResourceKeyKind;
+using ResourceKey = Presentation::ResourceKey;
+using ResourceDictionary = Presentation::ResourceDictionary;
+
+} // namespace Aero
+
+namespace Aero::Media {
+
+using Geometry = ::Aero::Presentation::Geometry;
+
+} // namespace Aero::Media
