33450fa74935686c8deea653088eadec42fcbeb2
Max <smith.w2055@gmail.com>
2026-07-30 15:25:04 +0800

    add shapes semantic namespace entry

---
+++ include/Aero/Shapes.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Aero/Controls/Shapes.hpp>
+
+namespace Aero::Shapes {
+
+using Shape = ::Aero::Controls::Shape;
+using Rectangle = ::Aero::Controls::Rectangle;
+using Ellipse = ::Aero::Controls::Ellipse;
+
+} // namespace Aero::Shapes
