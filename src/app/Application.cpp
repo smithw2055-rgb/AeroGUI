789318e4df7118267990b3de27c8f8610f2c463b
Max <smith.w2055@gmail.com>
2026-07-30 15:32:58 +0800

    remove platform service ownership from application

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -1,7 +1,4 @@
 #include <Aero/Application.hpp>
-#include <Aero/Audio/Audio.hpp>
-
-#include <new>
 
 namespace Aero {
 namespace {
@@ -12,18 +9,6 @@ thread_local Application* currentApplication = nullptr;
 
 Application* Application::Current() noexcept {
     return currentApplication;
-}
-
-Base::Result<Audio::Engine*> Application::GetAudioEngine() noexcept {
-    if (audio_ != nullptr) return audio_;
-    Audio::Engine* created = new (std::nothrow) Audio::Engine();
-    if (created == nullptr) {
-        return Base::Status::Failure(
-            Base::ErrorCode::OutOfMemory,
-            "Audio engine allocation failed");
-    }
-    audio_ = created;
-    return audio_;
 }
 
 void Application::Shutdown(int exitCode) noexcept {
@@ -41,8 +26,6 @@ void Application::Attach(
 }
 
 void Application::Detach() noexcept {
-    delete audio_;
-    audio_ = nullptr;
     if (currentApplication == this) {
         currentApplication = nullptr;
     }
