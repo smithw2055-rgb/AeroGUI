9d49eb71fbb83a5f7b992a075bb534f6e7962374
Max <smith.w2055@gmail.com>
2026-07-31 11:09:57 +0800

    build: fix strict compiler diagnostics

--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -7,7 +7,15 @@
 #define MINIAUDIO_IMPLEMENTATION
 #define MA_NO_ENCODING
 #define MA_NO_GENERATION
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#endif
 #include <miniaudio.h>
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 #endif
 
 namespace Aero::Audio {
