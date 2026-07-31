1be796d76874179032c6c929b9c1219e55a09871
Max <smith.w2055@gmail.com>
2026-07-31 11:49:26 +0800

    refactor: establish provider implementation boundaries

--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -4,18 +4,9 @@
 #include <new>
 
 #if AERO_AUDIO_MINIAUDIO
-#define MINIAUDIO_IMPLEMENTATION
 #define MA_NO_ENCODING
 #define MA_NO_GENERATION
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#endif
 #include <miniaudio.h>
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
 #endif
 
 namespace Aero::Audio {
--- src/runtime/ImageRuntime.cpp
+++ src/runtime/ImageRuntime.cpp
@@ -15,18 +15,7 @@
 
 #define STBI_NO_STDIO
 #define STBI_ONLY_PNG
-#define STB_IMAGE_IMPLEMENTATION
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wunused-function"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#endif
 #include "stb_image.h"
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
 
 namespace Aero::Detail {
 namespace {
