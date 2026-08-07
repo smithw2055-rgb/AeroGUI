25c6eee21de36718bf8060ccaff412e6e04abc63
Max <smith.w2055@gmail.com>
2026-08-07 08:54:12 +0800

    S9-S10 hide window surface API

--- src/render/opengl33/OpenGL33Factories.cpp
+++ src/render/opengl33/OpenGL33Factories.cpp
@@ -11,19 +11,12 @@ Base::Result<Base::Ref<Aero::RenderDevice>> CreateOpenGL33Device(
     return ::Aero::Render::Detail::CreateOpenGL33Device(options, allocator);
 }
 
-Base::Result<Base::Ref<Aero::RenderTarget>>
-CreateOpenGL33RenderTarget(
+Base::Result<Base::Ref<Aero::RenderTarget>> CreateOpenGL33RenderTarget(
     Base::Ref<Aero::RenderDevice> device,
     const OpenGL33RenderTargetOptions& options,
     Base::IAllocator* allocator) noexcept {
     return ::Aero::Render::Detail::CreateOpenGL33EmbeddedSurface(
         std::move(device), options, allocator);
-}
-
-Base::Result<Base::Ref<Aero::RenderTarget>> CreateOpenGL33WindowSurface(
-    const OpenGL33WindowSurfaceOptions& options,
-    Base::IAllocator* allocator) noexcept {
-    return ::Aero::Render::Detail::CreateOpenGL33WindowSurface(options, allocator);
 }
 
 } // namespace Aero::Render
